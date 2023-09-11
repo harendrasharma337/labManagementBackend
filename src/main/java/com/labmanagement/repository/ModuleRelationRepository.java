b6b46829eb6d774751894b4b2c04739d372ed5c3
Akshant Rajput <akshantrajput2001@gmail.com>
2023-09-12 00:28:02 +0530

    Changes Done Upload student

--- src/main/java/com/labmanagement/repository/ModuleRelationRepository.java
+++ src/main/java/com/labmanagement/repository/ModuleRelationRepository.java
@@ -1,12 +1,18 @@
 package com.labmanagement.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.labmanagement.domain.ModuleRelation;
+import com.labmanagement.domain.Modules;
 import com.labmanagement.domain.User;
 
 public interface ModuleRelationRepository extends JpaRepository<ModuleRelation, Long> {
 
 	ModuleRelation findByUser(User user);
+
+	List<ModuleRelation> findAllByModules(Modules m);
+
 
 }
